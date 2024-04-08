23e63c37da7259f50e8495b81dafad033fbd731c
Kalyan Devara <kalyan.vza@gmail.com>
2024-04-08 21:15:31 +0530

    updated

--- src/suby/api.js
+++ src/suby/api.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://backend-nodejs-suby.onrender.com"
+// export const API_URL = "https://backend-nodejs-suby.onrender.com"
 
-// export const API_URL = "http://localhost:4000"
+export const API_URL = "http://localhost:4000"
